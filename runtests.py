aeea76d861021e6bdb2bd666dd5b8293e0403538
David Cramer <dcramer@gmail.com>
2010-11-22 11:26:10 -0800

    Initial commit

---
+++ runtests.py
@@ -0,0 +1,19 @@
+import unittest
+
+from disqusapi import DisqusAPI
+
+class DisqusAPITest(unittest.TestCase):
+    API_SECRET = 'b'*64
+
+    def test_setSecretKey(self):
+        api = DisqusAPI(api_secret='a')
+        self.assertEquals(api.api_secret, 'a')
+        api.setSecretKey('b')
+        self.assertEquals(api.api_secret, 'b')
+    
+    def test_users_listActivity(self):
+        api = DisqusAPI(self.API_SECRET)
+        api.users.listActivity(foo='bar')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='disqus-python',
+    version='.'.join(map(str, __import__('disqusapi').__version__)),
+    author='DISQUS',
+    author_email='david@disqus.com',
+    url='http://github.com/disqus/disqus-python',
+    description = 'Disqus API Bindings',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['simplejson'],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
