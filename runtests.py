88c18d23d57b4e5910bb7fdc6833e470f6fb3470
David Cramer <dcramer@gmail.com>
2010-11-23 14:19:24 -0800

    Update tests

--- runtests.py
+++ runtests.py
@@ -5,11 +5,23 @@ from disqusapi import DisqusAPI, APIError
 class DisqusAPITest(unittest.TestCase):
     API_SECRET = 'b'*64
 
-    def test_setSecretKey(self):
-        api = DisqusAPI(api_secret='a')
-        self.assertEquals(api.api_secret, 'a')
-        api.setSecretKey('b')
-        self.assertEquals(api.api_secret, 'b')
+    def test_setKey(self):
+        api = DisqusAPI('a')
+        self.assertEquals(api.key, 'a')
+        api.setKey('b')
+        self.assertEquals(api.key, 'b')
+
+    def test_setFormat(self):
+        api = DisqusAPI()
+        self.assertEquals(api.format, 'json')
+        api.setFormat('jsonp')
+        self.assertEquals(api.format, 'jsonp')
+    
+    def test_setVersion(self):
+        api = DisqusAPI()
+        self.assertEquals(api.version, '3.0')
+        api.setVersion('3.1')
+        self.assertEquals(api.version, '3.1')
     
     def test_users_listActivity(self):
         api = DisqusAPI(self.API_SECRET)
