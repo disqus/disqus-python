287a742c8570b6142808b33c52a894bc51816638
David Cramer <dcramer@gmail.com>
2011-11-01 17:30:39 -0700

    Version bump to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='disqus-python',
-    version='0.2.0',
+    version='0.3.0',
     author='DISQUS',
     author_email='david@disqus.com',
     url='http://github.com/disqus/disqus-python',
